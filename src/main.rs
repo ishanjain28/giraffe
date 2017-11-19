09b6bb7eb63edb41b8f86000dc19c97aec42e1e5
Ishan Jain <ishanjain28@gmail.com>
2017-11-19 23:13:26 +0530

    Using x11grab, Correctly takes a screenshot now

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,31 @@
-extern crate x11;
+extern crate x11cap;
 extern crate image;
 
-
-use std::ffi;
-use std::os::raw;
-use x11::xlib;
-use std::fmt;
-use std::mem;
-use std::slice;
-
-struct Display {
-    width: u32,
-    height: u32,
-    refer: *mut xlib::_XDisplay,
-}
+use image::{ImageBuffer, GenericImage};
 
 fn main() {
+    let c = x11cap::CaptureSource::Monitor(0);
 
-    let s = match ffi::CString::new(":0") {
-        Ok(v) => v,
-        Err(e) => {
-            println!("{}", e);
+    let mut d = x11cap::Capturer::new(c).unwrap();
 
-            return;
+    let f = d.capture_frame().unwrap();
+
+    let f = f.as_slice();
+
+
+    let mut img: image::RgbaImage = ImageBuffer::new(1920, 1080);
+
+
+    for i in 0..img.height() {
+        for j in 0..img.width() {
+
+            let p = f[(i * img.width() + j) as usize];
+
+            let q = image::Rgba { data: [p.r, p.g, p.b, 255] };
+
+            img.put_pixel(j, i, q);
         }
-    };
-
-    let d = Display::new(s);
-
-    let attribs = d.fetch_window_attributes();
-
-    let img = d.get_image();
-
-
-    let data;
-    unsafe {
-        data = slice::from_raw_parts(img.data, (img.bytes_per_line * img.height) as usize);
     }
 
-    //read &[i8] and convert to &[u8]
-    let mut u8_data: Vec<u8> = Vec::new();
-
-    for i in data {
-        u8_data.push(*i as u8);
-    }
-
-    let i = image::load_from_memory(u8_data.as_slice()).expect("Failed to read image");
-
-
-
-}
-
-
-impl Display {
-    fn new(s: std::ffi::CString) -> Display {
-
-        let x_img_n_ptr: *mut x11::xlib::XImage;
-
-        let display = open_display(s.as_ptr());
-        let h: u32;
-        let w: u32;
-
-        unsafe {
-            w = xlib::XDisplayWidth(display, 0) as u32;
-            h = xlib::XDisplayHeight(display, 0) as u32;
-        }
-        let d = Display {
-            height: h,
-            width: w,
-            refer: display,
-        };
-        d
-    }
-
-    fn get_resolution(&self) -> (u32, u32) {
-        (self.width, self.height)
-    }
-
-    fn fetch_window_attributes(&self) -> xlib::XWindowAttributes {
-        let mut window_attribs;
-
-        unsafe {
-            window_attribs = mem::uninitialized();
-            let window = xlib::XRootWindow(self.refer, 0);
-
-            let status = xlib::XGetWindowAttributes(self.refer, window, &mut window_attribs);
-            //println!("Window Attributes Status: {}", status);
-        };
-        window_attribs
-    }
-
-    fn get_image(&self) -> xlib::XImage {
-
-        let mut img;
-        unsafe {
-            img = *(xlib::XGetImage(
-                self.refer,
-                xlib::XDefaultRootWindow(self.refer),
-                0,
-                0,
-                self.width,
-                self.height,
-                xlib::XAllPlanes(),
-                xlib::ZPixmap,
-            ));
-        };
-        img
-    }
-}
-
-impl fmt::Display for Display {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?} {}x{}", self.refer, self.width, self.height)
-    }
-}
-
-fn create_pixmap(d: xlib::Display) {}
-
-fn open_display(d: *const raw::c_char) -> *mut x11::xlib::Display {
-    let display: *mut x11::xlib::Display;
-    unsafe {
-        display = x11::xlib::XOpenDisplay(d);
-    }
-
-    display
+    img.save("t.png");;
 }
